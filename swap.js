01f6e9d8a3979ef1f744a7b090e798edf7769179
anamerasul <anamerasul@gmail.com>
2022-02-01 16:55:26 +0600

    swap

---
+++ swap.js
@@ -0,0 +1,18 @@
+var first =5;
+
+var second =7;
+
+console.log(first,second);
+// first approch
+// var temp = first;
+
+// first =second;
+// second =temp;
+// console.log(first,second);
+
+// distructing
+
+[first, second]=[second,first];
+console.log(first,second);
+
+
