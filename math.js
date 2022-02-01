0b9502c371d9f74ef46778a67c68a87711f61e59
anamerasul <anamerasul@gmail.com>
2022-02-01 16:42:58 +0600

    math

---
+++ math.js
@@ -0,0 +1,21 @@
+// const MyNumber =-5;
+// const output=Math.abs(MyNumber);
+
+const MyNumber=11.5398;
+
+// const output =Math.ceil(MyNumber);
+// const output =Math.floor(MyNumber);
+// const output =Math.round(MyNumber);
+// const output =Math.random()*6;
+
+// const rounded =Math.round(output);
+
+// console.log(rounded);
+
+for(let i=0; i<=30 ;i++){
+               const output =Math.random()*6;
+
+               const rounded =Math.round(output);
+
+               console.log(rounded);          
+}
